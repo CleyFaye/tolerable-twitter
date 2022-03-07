fe7c671552a6ec4ae552c1e1b1ea92526fc42fad
CleyFaye <cleyfaye+github@gmail.com>
2022-03-07 23:55:09 +0100

    Version bump

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Alter twitter interface in some capacity
 // @namespace    https://github.com/CleyFaye/tolerable-twitter
-// @version      0.1
+// @version      0.1.1
 // @description  Try to remove/change some of twitter UI to be less annoying.
 // @author       Gabriel Paul "Cley Faye" Risterucci
 // @match        https://twitter.com/*
